f13f8ea67e7d9957ddbb0f1d8c789bbb2b88cae6
doc2dev <emkar2010@gmail.com>
2022-07-16 14:17:43 +0300

    Big Bang 💥

---
+++ app/src/main/java/ke/eston/soccerfixtures/MainActivity.kt
@@ -0,0 +1,35 @@
+package ke.eston.soccerfixtures
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ke.eston.soccerfixtures.ui.theme.SoccerFixturesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SoccerFixturesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
